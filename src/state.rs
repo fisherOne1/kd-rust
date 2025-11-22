dee507544f80f49a28943e8816e37c41315b3b96
fisher <weichier520@gmail.com>
2025-11-22 22:57:09 +0800

    Feat: 实现 HTTP 代理功能，支持 http 和 https 代理

--- src/state.rs
+++ src/state.rs
@@ -17,11 +17,27 @@ pub struct AppState {
 
 impl AppState {
     pub fn new(db: Connection, config: Config) -> Result<Self, KdError> {
-        let http_client = Client::builder()
+        let mut client_builder = Client::builder()
             .pool_max_idle_per_host(10)
             .pool_idle_timeout(std::time::Duration::from_secs(30))
-            .user_agent("kd/0.1.0")
-            .build()?;
+            .user_agent("kd/0.1.0");
+
+        // Configure HTTP/HTTPS proxy if specified
+        if let Some(proxy_url) = &config.http_proxy {
+            if !proxy_url.is_empty() {
+                // Support both http:// and https:// proxy URLs
+                let proxy = if proxy_url.starts_with("https://") {
+                    reqwest::Proxy::https(proxy_url)
+                        .map_err(|e| KdError::Config(format!("Invalid HTTPS proxy URL: {}", e)))?
+                } else {
+                    reqwest::Proxy::http(proxy_url)
+                        .map_err(|e| KdError::Config(format!("Invalid HTTP proxy URL: {}", e)))?
+                };
+                client_builder = client_builder.proxy(proxy);
+            }
+        }
+
+        let http_client = client_builder.build()?;
 
         Ok(Self {
             db: Arc::new(db),
